128be3d79089fe66ffb6e3926940ee0e121f6930
Luke Maupin <lukecmaupin@gmail.com>
2021-11-02 09:55:41 -0400

    Adds base answers to app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // 1
+setTimeout(() => {
+    const div1 = document.querySelector(`#first`);
+    const p1 = document.createElement(`p`);
+    p1.innerText = `Hi`;
+    div1.append(p1);
+}, 1000);
+
+
+// 2
+setTimeout(() => {
+    const div2 = document.querySelector(`#timeout-nesting`);
+    const p2 = document.createElement(`p`);
+    p2.innerText = `One`;
+    div2.append(p2);
+    setTimeout (() => {
+        const p3 = document.createElement(`p`);
+        p3.innerText = `Two`;
+        div2.append(p3);
+    }, 1000);
+}, 2000);
+
+// 3a
+let counter = 1;
+let time = setInterval(() => {
+    console.log(counter);
+    counter++
+}, 1000);
+
+// 3b
+const stop = document.querySelector(`button`);
+stop.addEventListener(`click`, () => {
+    clearInterval(time);
+});
